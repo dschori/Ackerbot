148cc3d1ce63fe31f29dc6983b8ffaad1527a42e
Damian Schori <damian.schori@gmail.com>
2020-11-29 21:02:22 +0100

    update

--- catkin_ws/src/robot_pkg/src/selfie_race/scripts/talker.py
+++ catkin_ws/src/robot_pkg/src/selfie_race/scripts/talker.py
@@ -1,40 +1,21 @@
 #!/usr/bin/env python3
 import rospy
-import roslib
-from geometry_msgs.msg import Twist
+from sensor_msgs.msg import PointCloud2
+import random
 
-from adafruit_servokit import ServoKit
-kit = ServoKit(channels=16)
+def callback(data, pub):
+    # Here would be a place to do some sort of computation to decide if you want
+    # the data republished on the new topic. I've inserted a dummy computation.:
+    if random.randint(1,10) <= 5:
+        rospy.loginfo("republish this cloud!")
+        pub.publish(data)
+    return
 
-rospy.init_node('control_subscriver')
-
-r = rospy.Rate(1.0)
-
-
-class Control:
-    def __init__(self):
-        self.pos_x, self.pos_y = 0., 0.
-        self.ori_x, self.ori_y, self.ori_z, self.ori_w = 0., 0., 0., 0.
-        self.vel_lin_x, self.vel_lin_y, self.vel_lin_z = 0., 0., 0.
-        self.vel_ang_x, self.vel_ang_y, self.vel_ang_z = 0., 0., 0.
-
-    def keyboard_twist(self, msg):
-
-        self.vel_lin_x = msg.twist.twist.linear.x
-        self.vel_lin_y = msg.twist.twist.linear.y
-        self.vel_lin_z = msg.twist.twist.linear.z
-
-        self.vel_ang_x = msg.twist.twist.angular.x
-        self.vel_ang_y = msg.twist.twist.angular.y
-        self.vel_ang_z = msg.twist.twist.angular.z
-
-        kit.continuous_servo[15].throttle = self.vel_lin_x
-
-    def listener(self):
-        rospy.Subscriber("/cmd_vel", Twist, self.keyboard_twist)
-        rospy.sleep(0.01)
-
+def listener():
+    rospy.init_node('pointcloud_republisher')
+    pub = rospy.Publisher("pcd_points", PointCloud2, queue_size=1)
+    rospy.Subscriber("depth/points", PointCloud2, callback, callback_args=(pub))
+    rospy.spin()
 
 if __name__ == '__main__':
-    c = Control()
-    c.listener()
+    listener()
