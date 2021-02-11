a4df7040fa5ff4b768944fa53ce45d53679fb908
damian.schori <dschori@hsr.ch>
2021-02-11 14:09:13 +0100

    update

--- ackerbot_sim_ws/src/rl-scouting/scouting_train/scripts/images_to_video.py
+++ ackerbot_sim_ws/src/rl-scouting/scouting_train/scripts/images_to_video.py
@@ -1,10 +1,10 @@
 import cv2
 import os
 
-nr = 35
+nr = 298
 
-image_folder = 'C:/Users/damian.schori/Downloads/training/check_{}'.format(nr)
-video_name = 'C:/Users/damian.schori/Downloads/training/check_{}.avi'.format(nr)
+image_folder = 'C:/Users/damian.schori/switchdrive/Master-Masterarbeit/50_Data/Videos/rl_learning_home1/check_{}/images'.format(nr)
+video_name = 'C:/Users/damian.schori/switchdrive/Master-Masterarbeit/50_Data/Videos/rl_learning_home1/check_{}/vid_{}.avi'.format(nr, nr)
 
 images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
 frame = cv2.imread(os.path.join(image_folder, images[0]))
